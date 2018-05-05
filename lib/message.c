040ed380ec72cc10607837a4621bab21ac1f87e0
Florian Stinglmayr <florian@n0la.org>
2018-05-05 20:39:35 +0200

    convenience function to check arguments

--- lib/message.c
+++ lib/message.c
@@ -303,6 +303,13 @@ bool irc_message_is(irc_message_t m, char const *cmd)
     return (strcmp(m->command, cmd) == 0);
 }
 
+bool irc_message_arg_is(irc_message_t m, int idx, char const *what)
+{
+    return_if_true(m == NULL, false);
+    return_if_true(idx >= m->argslen || idx < 0, false);
+    return (strcmp(m->args[idx], what) == 0);
+}
+
 bool irc_message_prefix_nick(irc_message_t m, char const *nick)
 {
     char *sep = NULL;
